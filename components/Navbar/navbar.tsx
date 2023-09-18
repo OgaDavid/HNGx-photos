6bda9b16428bbb7dd5ca70c1b8369ac5107f3d20
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-09-18 10:42:01 -1100

    feat: responsive navbar

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,62 +1,126 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Container from "../ui/container";
 import Logo from "./logo";
 import UserAvatar from "./avatar";
 import Link from "next/link";
 import Hamburger from "./hamburger";
+import { cn } from "@/lib/utils";
+import { ArrowUpRight } from "lucide-react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  function openNavigation() {
+    setIsOpen(!isOpen);
+  }
+  function closeNavigation() {
+    setIsOpen(false);
+  }
   return (
-    <nav className="border-b border-b-gray-200 shadow-sm md:shadow-md py-4">
-      <Container>
-        <span className="flex items-center justify-between">
-          <div className="flex items-center gap-7">
-            <Logo />
-            <span className="max-md:hidden">
-              <ul className="flex items-center text-sm font-medium gap-4">
-                <li>
-                  <Link
-                    className="hover:text-[#103FEF] transition duration-300"
-                    href="/gallery"
-                  >
-                    Gallery
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-[#103FEF] transition duration-300"
-                    href="/upload"
-                  >
-                    Upload
-                  </Link>
-                </li>
-              </ul>
-            </span>
-          </div>
-          <div className="flex items-center gap-10">
-            <span className="flex gap-2 max-md:hidden items-center">
-              <Link
-                className="py-[6px] transition duration-200 font-medium text-sm px-5 hover:bg-gray-100 rounded-full "
-                href="/sign-in"
-              >
-                Login
-              </Link>
-              <Link
-                className="py-[6px] transition duration-200 font-medium text-white bg-[#103FEF] text-sm px-5 hover:bg-opacity-60 rounded-full "
-                href="/sign-up"
-              >
-                Sign up
-              </Link>
-            </span>
-            <div className="flex gap-5 items-center">
-              <UserAvatar />
-              <span className="md:hidden">
-                <Hamburger open={false} />
+    <>
+      <header
+        className={cn(
+          "fixed w-full md:shadow-md py-3 md:py-4",
+          !isOpen && "border-b shadow-sm border-b-gray-200 "
+        )}
+      >
+        <Container>
+          <span className="flex items-center justify-between">
+            <div className="flex items-center gap-7">
+              <Logo />
+              <nav className="max-md:hidden">
+                <ul className="flex items-center text-sm font-medium gap-4">
+                  <li>
+                    <Link
+                      className="hover:text-[#103FEF] text-[#202124] flex items-start gap-[2px] transition duration-300"
+                      href="/gallery"
+                    >
+                      Gallery
+                      <ArrowUpRight className="w-3 h-3"/>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="hover:text-[#103FEF] text-[#202124] flex items-start gap-[2px] transition duration-300"
+                      href="/upload"
+                    >
+                      Upload
+                      <ArrowUpRight className="w-3 h-3"/>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="flex items-center gap-10">
+              <span className="flex gap-2 max-md:hidden items-center">
+                <Link
+                  className="py-2 transition duration-200 font-medium text-sm px-6 hover:bg-gray-100 rounded-[4px]"
+                  href="/sign-in"
+                >
+                  Login
+                </Link>
+                <Link
+                  className="py-2 transition duration-200 font-medium text-white bg-[#103FEF] text-sm px-6 hover:bg-opacity-60 rounded-[4px]"
+                  href="/sign-up"
+                >
+                  Sign up
+                </Link>
+              </span>
+              <div className="flex gap-5 justify-center items-center">
+                <UserAvatar />
+                <span onClick={openNavigation} className="md:hidden mb-2">
+                  <Hamburger open={isOpen} />
+                </span>
+              </div>
+            </div>
+          </span>
+        </Container>
+
+        {/* MOBILE NAVIGATION */}
+        <Container>
+          <div className="bg-white shadow-md rounded-lg transition duration-150">
+            <div className={cn(isOpen ? "block" : "hidden", "pl-4")}>
+              <nav>
+                <ul className="text-sm py-10 flex flex-col items-start gap-5 font-medium">
+                  <li>
+                    <Link
+                      className="text-[#103FEF] border-b flex items-start gap-[2px] transition duration-300"
+                      href="/gallery"
+                    >
+                      Gallery
+                      <ArrowUpRight className="w-3 h-3"/>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-[#103FEF] flex items-start gap-[2px] transition duration-300"
+                      href="/upload"
+                    >
+                      Upload
+                      <ArrowUpRight className="w-3 h-3"/>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <span className="flex pb-5 gap-2 justify-center">
+                <Link
+                  className="py-2 transition duration-200 font-medium text-sm px-6 hover:bg-gray-100 rounded-[4px]"
+                  href="/sign-in"
+                >
+                  Login
+                </Link>
+                <Link
+                  className="py-2 transition duration-200 font-medium text-white bg-[#103FEF] text-sm px-6 hover:bg-opacity-60 rounded-[4px]"
+                  href="/sign-up"
+                >
+                  Sign up
+                </Link>
               </span>
             </div>
           </div>
-        </span>
-      </Container>
-    </nav>
+        </Container>
+      </header>
+    </>
   );
 }
