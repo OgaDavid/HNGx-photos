b70d9fc3f4d06760f9aa61ea6633e2a6ae958907
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-09-18 19:13:53 -1100

    add: password recommendation

--- app/(routes)/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(routes)/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,19 +1,31 @@
 import { SignIn } from "@clerk/nextjs";
 import type { Metadata } from "next";
+import { ArrowRight } from "lucide-react";
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: "Sign in | Photos",
-  description: "Login to your account"
-}
+  description: "Login to your account",
+};
 
 export default function Page() {
   return (
-    <SignIn
-      appearance={{
-        elements: {
-          rootBox: "mx-auto pt-[70px]",
-        },
-      }}
-    />
+    <>
+      <SignIn
+        appearance={{
+          elements: {
+            rootBox: "mx-auto pt-[70px]",
+          },
+        }}
+      />
+      <span className="flex flex-col text-sm text-[#202124] font-medium items-center mt-10 md:mt-4 justify-center">
+        <h2 className="flex pb-1 items-center gap-1">
+          Are you a test user? login with <ArrowRight className="w-3 h-3" />
+        </h2>
+        <div className="">
+          <p>Email: <span className="underline text-[#103FEF]">user@example.com</span></p>
+          <p>Password: <span className="underline text-[#103FEF]">1Password</span></p>
+        </div>
+      </span>
+    </>
   );
 }
