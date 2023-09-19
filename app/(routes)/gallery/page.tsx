c60ef13b1dcfcd3ae7faf2f1f52849e8b8a56292
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-09-18 21:07:39 -1100

    feat: setup cloudinary

--- app/(routes)/gallery/page.tsx
+++ app/(routes)/gallery/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import Container from "@/components/ui/container";
+import React from "react";
+import cloudinary from "cloudinary";
 
-export default function GalleryPage() {
+export default async function GalleryPage() {
+  const images = await cloudinary.v2.search
+    .expression(
+      "resource_type:image"
+    )
+    .sort_by("public_id", "desc")
+    .max_results(30)
+    .execute();
+
+    console.log(images)
   return (
-    <main>Gallery page</main>
-  )
+    <main>
+      <Container>Gallery page</Container>
+    </main>
+  );
 }
