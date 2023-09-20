db61ba1dbfd68e5aebd6ca527c839f0b4f9a5e2b
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-09-20 10:37:08 -1100

    feat: mad search and filtering functionality

--- components/Gallery/gallery.tsx
+++ components/Gallery/gallery.tsx
@@ -34,6 +34,10 @@ export default function Gallery() {
   };
 
   const handleSearch = (tag: string) => {
+    if (tag === "") {
+      setImages(imageData);
+      return;
+    }
     const filteredImages = imageData.filter((image) =>
       image.tags.includes(tag)
     );
@@ -59,6 +63,7 @@ export default function Gallery() {
             className="md:max-w-[300px]"
             onChange={(e) => setTagName(e.currentTarget.value)}
             id="tag-name"
+            placeholder="Search image by tag..."
             value={tagName}
           />
           <Button type="submit">Search</Button>
@@ -68,7 +73,11 @@ export default function Gallery() {
       <div className="flex items-center gap-2 flex-wrap my-1">
         {tags.map((tag) => (
           <span
-            className="px-2 py-[6px] rounded-md bg-gray-200 text-black"
+            onClick={() => {
+              setTagName(tag.tagName);
+              handleSearch(tag.tagName);
+            }}
+            className="px-2 cursor-pointer py-[6px] rounded-md bg-gray-100 text-black"
             key={tag.id}
           >
             {tag.tagName}
@@ -78,44 +87,50 @@ export default function Gallery() {
 
       {/* IMAGES SECTION */}
       <section className="pt-10">
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="grid">
-            {(provided) => (
-              <section
-                className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-3"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {images.map((image, idx) => (
-                  <Draggable
-                    key={image.id}
-                    draggableId={idx.toString()}
-                    index={idx}
-                  >
-                    {(provided) => (
-                      <article
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                        className="flex relative group pb-[15px] transition duration-300 flex-col items-center"
-                      >
-                        <GripVertical className="absolute top-2 right-2 w-4 h-4 hidden group-hover:flex" />
-                        <Image
-                          className="w-[200px] h-[200px] object-cover rounded-md"
-                          alt="image"
-                          src={image.url}
-                          width={300}
-                          height={300}
-                        />
-                      </article>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </section>
-            )}
-          </Droppable>
-        </DragDropContext>
+        {images.length === 0 ? (
+          <div className="flex py-20 items-center justify-center">
+            <h2 className="text-xl font-medium">👀 No images found.</h2>
+          </div>
+        ) : (
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="grid">
+              {(provided) => (
+                <section
+                  className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-3"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {images.map((image, idx) => (
+                    <Draggable
+                      key={image.id}
+                      draggableId={idx.toString()}
+                      index={idx}
+                    >
+                      {(provided) => (
+                        <article
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                          className="flex relative group pb-[15px] transition duration-300 flex-col items-center"
+                        >
+                          <GripVertical className="absolute text-gray-100 top-2 right-2 w-5 h-5 md:hidden md:group-hover:flex" />
+                          <Image
+                            className="w-[200px] h-[200px] object-cover rounded-md"
+                            alt="image"
+                            src={image.url}
+                            width={300}
+                            height={300}
+                          />
+                        </article>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </section>
+              )}
+            </Droppable>
+          </DragDropContext>
+        )}
       </section>
     </>
   );
