bc0218d149120bda087fbe9e73804801e89591b2
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-09-19 16:35:09 -1100

    feat: drag and drop feature

--- app/(routes)/gallery/components/masnory-grid.tsx
+++ app/(routes)/gallery/components/masnory-grid.tsx
@@ -1,31 +1,74 @@
 "use client";
-import Masonry from "react-masonry-css";
 
-import React from "react";
+import Masonry from "react-masonry-css";
 import CloudinaryImage from "./cloudinary-image";
+import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
+import { useEffect, useState } from "react";
 
 export default function MasnoryGrid({
   imagesResult,
 }: {
   imagesResult: ImageResult[];
 }) {
+  const [images, updateImages] = useState(imagesResult);
+
+  useEffect(() => {
+    updateImages(images);
+  }, [imagesResult]);
+
+  const handleOnDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+
+    const draggableImages = [...images];
+
+    const [reorderedImages] = draggableImages.splice(result.source.index, 1);
+
+    draggableImages.splice(result.destination!!.index, 0, reorderedImages);
+
+    updateImages(draggableImages);
+  };
+
+  //   masnory breakpoints
   const breakpointColumns = {
     default: 5,
     1100: 4,
     700: 3,
     500: 2,
   };
+
   return (
-    <Masonry
-      breakpointCols={breakpointColumns}
-      className="my-masonry-grid"
-      columnClassName="my-masonry-grid_column"
-    >
-      {imagesResult.map((image) => (
-        <div key={image.public_id} className="flex flex-col items-center">
-          <CloudinaryImage publicId={image.public_id} />
-        </div>
-      ))}
-    </Masonry>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="masnory_grid">
+        {(provided) => (
+          <section {...provided.droppableProps} ref={provided.innerRef}>
+            <Masonry
+              breakpointCols={breakpointColumns}
+              className="my-masonry-grid"
+              columnClassName="my-masonry-grid_column"
+            >
+              {imagesResult.map((image, idx) => (
+                <Draggable
+                  key={image.public_id}
+                  draggableId={idx.toString()}
+                  index={idx}
+                >
+                  {(provided) => (
+                    <article
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                      className="flex pb-[15px] flex-col items-center"
+                    >
+                      <CloudinaryImage publicId={image.public_id} />
+                    </article>
+                  )}
+                </Draggable>
+              ))}
+            </Masonry>
+            {provided.placeholder}
+          </section>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
