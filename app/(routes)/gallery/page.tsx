aa6ed7f8c10b1ce1bb631f7aaf85a5a924a8c9b3
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-09-20 07:09:09 -1100

    feat: updated mad things 🙌

--- app/(routes)/gallery/page.tsx
+++ app/(routes)/gallery/page.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
   description: "Check out your Gallery.",
 };
 
+const MAX_COLUMNS = 5;
+
 export default async function GalleryPage({
   searchParams: { search },
 }: {
@@ -27,6 +29,12 @@ export default async function GalleryPage({
     .max_results(30)
     .execute()) as { resources: ImageResult[] };
 
+  function getColumns(colIndex: number) {
+    return imagesResult.resources.filter(
+      (resource, idx) => idx % MAX_COLUMNS === colIndex
+    );
+  }
+
   return (
     <main>
       <Container>
@@ -36,24 +44,50 @@ export default async function GalleryPage({
             Showing {imagesResult.resources.length} images.
           </h1>
         </span>
-        <MasnoryGrid imagesResult={imagesResult.resources} />
-        {/* {imagesResult.resources.map((image, idx) => (
-          <article key={idx} className="flex relative group pb-[15px] transition duration-300 flex-col items-center">
-            <CloudinaryImage
-              imageData={image}
-              tags={image.tags}
-              publicId={image.public_id}
-              // onUnheart={(unheartedImage: ImageResult) => {
-              //   updateImages((currentResources: ImageResult[]) =>
-              //     currentResources.filter(
-              //       (resource) =>
-              //         resource.public_id !== unheartedImage.public_id
-              //     )
-              //   );
-              // }}
-            />
-          </article>
-        ))} */}
+
+        <div className="grid grid-cols-5 gap-3">
+          {[
+            getColumns(0),
+            getColumns(1),
+            getColumns(2),
+            getColumns(3),
+            getColumns(4),
+          ].map((column, idx) => (
+            <section key={idx} className="flex flex-col gap-3">
+              {column.map((image) => (
+                <CloudinaryImage
+                  imageData={image}
+                  tags={image.tags}
+                  publicId={image.public_id}
+                />
+              ))}
+            </section>
+          ))}
+        </div>
+
+        {/* <MasnoryGrid imagesResult={imagesResult.resources} /> */}
+        {/* <section className="grid md:grid-cols-5 gap-3">
+          {imagesResult.resources.map((image, idx) => (
+            <article
+              key={idx}
+              className="flex relative group pb-[15px] transition duration-300 flex-col items-center"
+            >
+              <CloudinaryImage
+                imageData={image}
+                tags={image.tags}
+                publicId={image.public_id}
+                // onUnheart={(unheartedImage: ImageResult) => {
+                //   updateImages((currentResources: ImageResult[]) =>
+                //     currentResources.filter(
+                //       (resource) =>
+                //         resource.public_id !== unheartedImage.public_id
+                //     )
+                //   );
+                // }}
+              />
+            </article>
+          ))}
+        </section> */}
       </Container>
     </main>
   );
